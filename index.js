eed464440a522096932ff6121e49a2d9220f11b6
Tony <tonylehuynh66@gmail.com>
2023-06-09 12:05:35 +1000

    variables initialised

---
+++ index.js
@@ -0,0 +1,9 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+

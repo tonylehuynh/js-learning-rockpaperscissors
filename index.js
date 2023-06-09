7f4f867efc4932da2dd2d79707c83b40f27a5c5a
Tony <tonylehuynh66@gmail.com>
2023-06-09 12:09:45 +1000

    computerTurn random function complete

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ choiceBtns.forEach(button => button.addEventListener("click"), () => {
 	
 	player = button.textContent;
 })
+
+function computerTurn(){
+	const randNum = Math.floor(Math.random() * 3) + 1;
+
+	switch(randNum){
+		case 1:
+			computer = "ROCK";
+			break;
+		case 2:
+			computer = "PAPER";
+			break;
+		case 3:
+			computer = "SCISSORS";
+			break;
+	}
+}
